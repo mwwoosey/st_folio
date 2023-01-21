b80c9cc352f61edc822e16b84f0c867163a853cd
mwwoosey <woosey@gmail.com>
2023-01-21 19:37:19 +0000

    updating with the data from the tutorial

--- script.py
+++ script.py
@@ -1,14 +1,31 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
+
+# https://docs.streamlit.io/library/get-started/create-an-app
 st.write("""
 # My first app
 Hello *world!*
 """)
 
-choc_freq = range(1,25)
-kellie_happy = [x**2 for x in choc_freq]
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
 d = {'Chocolate_Amount': choc_freq, 'Kellie_Happiness': kellie_happy}
 df = pd.DataFrame(data=d)
 st.line_chart(df)
+
+st.write(df)
+
+st.subheader('Number of kisses by hour')
+
