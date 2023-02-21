9753e14da15e40a16b9377943f5c748956ffe4f2
mwwoosey <woosey@gmail.com>
2023-02-21 13:54:04 +0000

    updated to include some containers and images

--- script.py
+++ script.py
@@ -1,31 +1,55 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from PIL import Image
+import requests
+from streamlit_lottie import st_lottie
 
+st.set_page_config(page_title="My Webpage", page_icon="@:tada:", layout="wide")
 
-# https://docs.streamlit.io/library/get-started/create-an-app
-st.write("""
-# My first app
-Hello *world!*
-""")
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# load assets
+lottie = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_Jj1h5xMtEv.json")
+img_mountain = Image.open("images/IMG_0306.jpg")
 
+# header section
+with st.container():
+    st.subheader("Hi, I'm Michael :wave:")
+    st.title("A Data Analyst based in the UK")
+    st.write("I am passionate about finding ways to use Python and VBA to be more effective in business")
+    st.write("[Learn More >](https://google.com)")
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+# what i do
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
 
-d = {'Chocolate_Amount': choc_freq, 'Kellie_Happiness': kellie_happy}
-df = pd.DataFrame(data=d)
-st.line_chart(df)
+    with left_column:
+        st.header("What I do")
+        st.write("""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius, odio a lacinia auctor, massa massa pellentesque diam, a aliquet neque urna vitae magna. In feugiat, enim efficitur consequat blandit, risus nisi suscipit turpis, sit amet pharetra nisl tellus eget nunc. Quisque auctor erat dictum dolor efficitur varius. Praesent lacinia felis sit amet dolor ultrices suscipit scelerisque quis justo. Etiam vitae nulla malesuada, tristique neque ut, dictum lorem. Curabitur justo magna, aliquam sit amet tincidunt pulvinar, blandit ac dui. Nulla sed mauris sed felis convallis rutrum vitae nec elit.
+        Praesent pharetra sapien sit amet malesuada tempus. Proin dapibus est lorem, id vestibulum lorem elementum at. Suspendisse id interdum leo, feugiat vehicula turpis. Morbi ac erat aliquam, fermentum lectus eu, posuere mauris. Fusce ac turpis quis arcu sagittis ullamcorper. Vivamus molestie vestibulum est sit amet semper. Sed sed ligula non eros ultrices accumsan. Maecenas condimentum volutpat erat quis mollis. Morbi cursus, dui sed dapibus vulputate, risus nisl laoreet lacus, rhoncus fermentum eros lorem ac nulla. Praesent congue enim id faucibus auctor.
+        """)
+        st.write("[LinkedIn >](http://linkedin.com)")
 
-st.write(df)
+    with right_column:
+        st_lottie(lottie, height=300, key="coding")
 
-st.subheader('Number of kisses by hour')
+# my first, first project
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1,2))
+    with image_column:
+        st.image(img_mountain)
 
+    with text_column:
+        st.subheader("Something blah")
+        st.write("""
+        Here is some information about something I did
+        """)
+        st.markdown("[Watch Video...](http://google.com)")
